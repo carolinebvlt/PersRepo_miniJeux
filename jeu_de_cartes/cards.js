842bfe9a5e29c32fa12a60ac1fcd04e4bce95ed2
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2018-01-04 22:36:59 +0100

    6players

--- jeu_de_cartes/cards.js
+++ jeu_de_cartes/cards.js
@@ -1,16 +1,62 @@
-var player1 = {};
-var player2 = {};
-var players = [player1, player2];
+var player1 = {}, player2 = {}, player3 = {},
+    player4 = {}, player5 = {}, player6 = {};
+var players = [player1, player2, player3, player4, player5, player6];
 var flop1, flop2, flop3, turn, river;
+var Flop1 = document.getElementById('Flop1');
+var Flop2 = document.getElementById('Flop2');
+var Flop3 = document.getElementById('Flop3');
+var Turn = document.getElementById('Turn');
+var River = document.getElementById('River');
+var Flop1Nbr = document.getElementById('Flop1Nbr');
+var Flop2Nbr = document.getElementById('Flop2Nbr');
+var Flop3Nbr = document.getElementById('Flop3Nbr');
+var TurnNbr = document.getElementById('TurnNbr');
+var RiverNbr = document.getElementById('RiverNbr');
+var pers1Card1 = document.getElementById('pers1Card1');
+var pers1Card2 = document.getElementById('pers1Card2');
+var pers2Card1 = document.getElementById('pers2Card1');
+var pers2Card2 = document.getElementById('pers2Card2');
+var pers3Card1 = document.getElementById('pers3Card1');
+var pers3Card2 = document.getElementById('pers3Card2');
+var pers4Card1 = document.getElementById('pers4Card1');
+var pers4Card2 = document.getElementById('pers4Card2');
+var pers5Card1 = document.getElementById('pers5Card1');
+var pers5Card2 = document.getElementById('pers5Card2');
+var pers6Card1 = document.getElementById('pers6Card1');
+var pers6Card2 = document.getElementById('pers6Card2');
+var pers1Card1Nbr = document.getElementById('pers1Card1Nbr');
+var pers1Card2Nbr = document.getElementById('pers1Card2Nbr');
+var pers2Card1Nbr = document.getElementById('pers2Card1Nbr');
+var pers2Card2Nbr = document.getElementById('pers2Card2Nbr');
+var pers3Card1Nbr = document.getElementById('pers3Card1Nbr');
+var pers3Card2Nbr = document.getElementById('pers3Card2Nbr');
+var pers4Card1Nbr = document.getElementById('pers4Card1Nbr');
+var pers4Card2Nbr = document.getElementById('pers4Card2Nbr');
+var pers5Card1Nbr = document.getElementById('pers5Card1Nbr');
+var pers5Card2Nbr = document.getElementById('pers5Card2Nbr');
+var pers6Card1Nbr = document.getElementById('pers6Card1Nbr');
+var pers6Card2Nbr = document.getElementById('pers6Card2Nbr');
+var btnPlay = document.getElementById('btnPlay');
+var btns = document.getElementById('btns');
 
-/*---------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
 
 function play(){
+  btnPlay.style.display = "none";
+  btns.style.display = "block";
   var mc = mixCards();
   giveCards(mc);
   flop(mc);
   turn(mc);
   river(mc);
+  setCardsView(
+    [flop1, flop2, flop3, turn, river, player1.card1, player1.card2, player2.card1, player2.card2, player3.card1, player3.card2,
+     player4.card1, player4.card2, player5.card1, player5.card2, player6.card1, player6.card2,],
+    [Flop1, Flop2, Flop3, Turn, River, pers1Card1, pers1Card2, pers2Card1, pers2Card2, pers3Card1, pers3Card2,
+      pers4Card1, pers4Card2, pers5Card1, pers5Card2, pers6Card1, pers6Card2,],
+    [Flop1Nbr, Flop2Nbr, Flop3Nbr, TurnNbr, RiverNbr, pers1Card1Nbr, pers1Card2Nbr, pers2Card1Nbr, pers2Card2Nbr, pers3Card1Nbr, pers3Card2Nbr,
+      pers4Card1Nbr, pers4Card2Nbr, pers5Card1Nbr, pers5Card2Nbr, pers6Card1Nbr, pers6Card2Nbr])
   players.forEach(function(e){
     checkHand(e);
   });
@@ -18,11 +64,11 @@ function play(){
 
 /*--------------------------------DEALER-------------------------------------*/
 function mixCards(){
-  let cards = ["01a","01b","01c","01d","02a","02b","02c","02d","03a","03b","03c",
-    "03d","04a","04b","04c","04d","05a","05b","05c","05d","06a","06b","06c","06d",
-    "07a","07b","07c","07d","08a","08b","08c","08d","09a","09b","09c","09d","10a",
-    "10b","10c","10d","11a","11b","11c","11d","12a","12b","12c","12d",
-    "13a","13b","13c","13d"
+  let cards = ["01a","01b","01c","01d","02a","02b","02c","02d","03a",
+    "03b","03c","03d","04a","04b","04c","04d","05a","05b","05c","05d",
+    "06a","06b","06c","06d","07a","07b","07c","07d","08a","08b","08c",
+    "08d","09a","09b","09c","09d","10a","10b","10c","10d","11a","11b",
+    "11c","11d","12a","12b","12c","12d","13a","13b","13c","13d"
   ];
   let mixed = [];
   let i = 52;
@@ -39,104 +85,103 @@ function giveCards(mc){
   player1.card2 = mc[1];
   player2.card1 = mc[2];
   player2.card2 = mc[3];
+  player3.card1 = mc[4];
+  player3.card2 = mc[5];
+  player4.card1 = mc[6];
+  player4.card2 = mc[7];
+  player5.card1 = mc[8];
+  player5.card2 = mc[9];
+  player6.card1 = mc[10];
+  player6.card2 = mc[11];
 }
 function flop(mc){
-  flop1 = mc[5];
-  flop2 = mc[6];
-  flop3 = mc[7];
+  flop1 = mc[13];
+  flop2 = mc[14];
+  flop3 = mc[15];
 }
 function turn(mc){
-  turn = mc[9];
+  turn = mc[17];
 }
 function river(mc){
-  river = mc[11];
+  river = mc[19];
 }
 
-/*--------------------------CHECKHAND-------------------------------------------*/
+/*-------------------------------CARDSVIEW-----------------------------------*/
+function setCardsView(tab1, tab2, tab3){ // ["01c"], [divById], [asideById]
+  for (var i = 0; i < tab1.length; i++) {
+    console.log('tab1 = '+tab1[i]+ ' tab2 = '+tab2[i] + ' tab3 = '+tab3[i]);
+    let x = tab1[i].substr(2,1);
+    switch (x) {
+      case 'a': tab2[i].style.backgroundImage = "url('img/cards/carreau.png')"; break;
+      case 'b': tab2[i].style.backgroundImage = "url('img/cards/coeur.png')"; break;
+      case 'c': tab2[i].style.backgroundImage = "url('img/cards/pique.png')"; break;
+      case 'd': tab2[i].style.backgroundImage = "url('img/cards/trefle.png')"; break;
+    }
+    let z = tab1[i].substr(0,2); // => '01',.. ou '13'
+    tab3[i].innerHTML = z;
+  }
+}
+
+/*-------------------------------CHECKHAND-----------------------------------*/
 function checkHand(e){
   var sevenCards = [flop1, flop2, flop3, turn, river, e.card1, e.card2];
-  var cardsValues = [], cardsColors = [], countsValues =[], countsColors= [];
-  var countA = 0, countB = 0, countC = 0, countD = 0;
-  var count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0,
-      count6 = 0, count7 = 0, count8 = 0, count9 = 0, count10 = 0,
-      count11 = 0, count12 = 0, count13 = 0;
-  var sortedValues, sortedValuesInt = [];
+  var cardsValues = [], cardsColors = [];
+  var countsValues = {count1:0, count2:0, count3:0, count4:0,
+                      count5:0, count6:0, count7:0, count8:0,
+                      count9:0, count10:0, count11:0,
+                      count12:0, count13:0}
+  var countsColors = {countA:0, countB:0, countC:0, countD:0};
+  var sortedValuesInt = [];
 
-  sevenCards.forEach(function(e){
+  sevenCards.forEach(function(e){ // => cardsColors[] & cardsValues[]
     let x = e.substr(2,1);
     cardsColors.push(x);
     let y = e.substr(0,2);
     cardsValues.push(y);
   });
-  cardsColors.forEach(function(e){
+  cardsColors.forEach(function(e){ // => countsColors{}
     if(e == 'a')
-        countA++;
+        countsColors.countA++;
     if(e == 'b')
-        countB++;
+        countsColors.countB++;
     if(e == 'c')
-        countC++;
+        countsColors.countC++;
     if(e == 'd')
-        countD++;
+        countsColors.countD++;
   });
-  countsColors.push(countA);
-  countsColors.push(countB);
-  countsColors.push(countC);
-  countsColors.push(countD);
-  cardsValues.forEach(function(e){
+  cardsValues.forEach(function(e){ // => countsValues{}
     if(e == '01')
-        count1++;
+        countsValues.count1++;
     if(e == '02')
-        count2++;
+        countsValues.count2++;
     if(e == '03')
-        count3++;
+        countsValues.count3++;
     if(e == '04')
-        count4++;
+        countsValues.count4++;
     if(e == '05')
-        count5++;
+        countsValues.count5++;
     if(e == '06')
-        count6++;
+        countsValues.count6++;
     if(e == '07')
-        count7++;
+        countsValues.count7++;
     if(e == '08')
-        count8++;
+        countsValues.count8++;
     if(e == '09')
-        count9++;
+        countsValues.count9++;
     if(e == '10')
-        count10++;
+        countsValues.count10++;
     if(e == '11')
-        count11++;
+        countsValues.count11++;
     if(e == '12')
-        count12++;
+        countsValues.count12++;
     if(e == '13')
-        count13++;
+        countsValues.count13++;
   });
 
-  countsValues.push(count1);
-  countsValues.push(count2);
-  countsValues.push(count3);
-  countsValues.push(count4);
-  countsValues.push(count5);
-  countsValues.push(count6);
-  countsValues.push(count7);
-  countsValues.push(count8);
-  countsValues.push(count9);
-  countsValues.push(count10);
-  countsValues.push(count11);
-  countsValues.push(count12);
-  countsValues.push(count13);
-  sortedValues = cardsValues.sort();
-  sortedValues.forEach(function(e){
+  let sortedValues = cardsValues.sort();
+  sortedValues.forEach(function(e){ // => sortedValuesInt[]
     sortedValuesInt.push(Number(e));
   });
-
-  console.log('sevenCards = '+ sevenCards);
-  console.log('cardsValues = '+ cardsValues); // trié 3lignes plus haut
-  console.log('cardsColors = '+ cardsColors);
-  console.log('sortedValues = '+ sortedValues);
-  console.log('sortedValuesInt = '+ sortedValuesInt);
-  console.log('countsValues = '+ countsValues);
-  console.log('countsColors = '+ countsColors);
-  console.log('-------------------------');
 
   if(checkQuinteFlushRoyale(countsColors, sortedValuesInt)){
     console.log('QFR');
@@ -186,7 +231,8 @@ function checkHand(e){
     }
   }
 }
-/*-----------------------------CHECK----------------------------------------*/
+/*------------------functions-------------------*/
+// a corriger avec des for in
 function checkHauteur(tab){
   var iMax = tab.length-1;
   return tab[iMax];
@@ -194,6 +240,7 @@ function checkHauteur(tab){
 function checkPair(tab){
   var pair = false;
   for (var i = tab.length-1; i >= 0; i--) {
+    console.log('tab[i] = ' +tab[i]);
     if(tab[i]==2){
       pair = true;
       break;
@@ -215,7 +262,10 @@ function checkQuinte(tab){
   var quinte = false;
   let i = tab.length-1;
   while(i >= 0){
-    if((tab[i]==tab[i-1]+1)&&(tab[i-1]==tab[i-2]+1)&&(tab[i-2]==tab[i-3]+1)&&(tab[i-3]==tab[i-4]+1)) {
+    if((tab[i]==tab[i-1]+1)
+      &&(tab[i-1]==tab[i-2]+1)
+      &&(tab[i-2]==tab[i-3]+1)
+      &&(tab[i-3]==tab[i-4]+1)) {
       quinte = true;
       break;
     }
@@ -225,7 +275,10 @@ function checkQuinte(tab){
 }
 function checkCouleur(tab){
   var couleur = false;
-  if (tab[0] == 5 || tab[1] == 5 || tab[2] == 5 || tab[3] == 5) {
+  if (tab.countA == 5
+    || tab.countB == 5
+    || tab.countC == 5
+    || tab.countD == 5) {
     couleur = true;
   }
   return couleur;
@@ -241,11 +294,12 @@ function checkCarre(tab){
   return carre;
 }
 function checkRoyale(tab){
-  if( (tab.indexOf(1)!= -1)&&(tab.indexOf(10)!= -1)&&(tab.indexOf(11)!= -1)&&(tab.indexOf(12)!= -1)&&(tab.indexOf(13)!= -1) ){
+  if( (tab.indexOf(1)!= -1)&&(tab.indexOf(10)!= -1)
+  &&(tab.indexOf(11)!= -1)&&(tab.indexOf(12)!= -1)
+  &&(tab.indexOf(13)!= -1) ){
     return true;
   }
 }
-/*---------------------------------------------------------------------*/
 function checkDoublePair(tab){
  var pair = 0;
  for (var i = tab.length-1; i >= 0; i--) {
